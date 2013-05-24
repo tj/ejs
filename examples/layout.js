5e48312dd4ef2338b3a58f84b2c0125a4598f4e9
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2013-05-24 10:32:31 -0400

    adding blocks to render and adding err for when filename is not provided

--- examples/layout.js
+++ examples/layout.js
@@ -1,14 +1,13 @@
-
 /**
- * Module dependencies.
+ * Render File Example
  */
 
 var ejs = require('../');
 
-var opts = {
+console.log("RENDER FILE EXAMPLE:\n");
+var rf_opts = {
 	locals:{
-		listType: "names",
-		list: "song"
+		listType: "name"
 	},
 	blocks: {
 		list: {
@@ -23,7 +22,31 @@ var opts = {
 	}
 }
 
-ejs.renderFile("./layout.ejs", opts, function(err, html){
+ejs.renderFile("./layout.ejs", rf_opts, function(err, html){
 	console.log("ERR", err);
 	console.log("HTML", html);
 });
+
+/**
+ * Render Example
+ */
+console.log("\n\nRENDER EXAMPLE:\n");
+var r_opts = {
+	locals:{
+		listType: "name"
+	},
+	blocks: {
+		list: {
+			template: "<% if (names.length) { %><ul><% names.forEach(function(name){ %><li><%= name %></li><% }) %></ul><% } %>",
+			locals: {
+				names:[
+					"matthew",
+					"caitlin"
+				],
+			}
+		}
+	}
+}
+
+var r = ejs.render("<div><p>Here is a list of <%- listType %>s</p><%- list %></div>", r_opts);
+console.log(r);
