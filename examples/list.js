7f79048b08221b43dbb82954d54a60831a5de95b
Rohit Paul <113459757+RohitPaul0007@users.noreply.github.com>
2023-09-12 12:36:50 +0530

    Update list.js

--- examples/list.js
+++ examples/list.js
@@ -3,11 +3,11 @@
  * Module dependencies.
  */
 
-var ejs = require('../')
+let ejs = require('../')
   , fs = require('fs')
   , str = fs.readFileSync(__dirname + '/list.ejs', 'utf8');
 
-var ret = ejs.render(str, {
+let ret = ejs.render(str, {
   names: ['foo', 'bar', 'baz']
 });
 
